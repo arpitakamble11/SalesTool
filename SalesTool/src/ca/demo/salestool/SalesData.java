649ed5848e59aa41d98b3473229c52367f98da35
Arpita Kamble <arpita11kamble@yahoo.com>
2016-11-28 18:05:11 +0530

    Partial implementation of displaying the numbers

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -4,6 +4,9 @@ public class SalesData {
 int data[]={0,4,42};
 
 public void display(){
-	System.out.println("ToDo: Implement Data Display");
+	for(int i=0;i< data.length;i++){
+		System.out.println("");
+		
+	}
 }
 }
